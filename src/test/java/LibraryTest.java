a7b1660d8f9c74928f7f37b9dd6c8eded77c1fa7
Ifeyinwa Kanu <chika.kanu@procompletions.com>
2019-03-27 04:11:26 +0000

    Library - can add book and count test passed

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,17 +6,30 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
 
     private Library library;
-    private Book books;
+    private Book book;
 
     @Before
     public void before() {
         library = new Library(500);
-        books = new Book("Things Fall Apart", "Chinua Achebe", "Non Fiction");
+        book = new Book("Things Fall Apart", "Chinua Achebe", "Non Fiction");
     }
 
     @Test
     public void cancheckCapacity() {
         assertEquals(500, library.checkCapacity());
+    }
+
+    @Test
+    public void canAddBook() {
+        library.addBook(book);
+        assertEquals(1, library.bookCount());
+
+    }
+
+    @Test
+    public void shouldCountBook() {
+        library.addBook(book);
+        assertEquals(1, library.bookCount());
     }
 
 
